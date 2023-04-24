933874a775eb1aed4f81902b0b4c2f226d17c39c
juanRoa <juanjaroa@gmail.com>
2023-04-24 14:06:45 -0500

    added tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="app">
+<div id="calendar-week" class="container">
+<!-- Markup for calendar week -->
+<p class="text-red-500">holi!</p>
+</div>
+<div id="calendar-entry">
+<!-- Markup for calendar entry -->
+</div>
+</div>
+</template>
+<script>
+export default {
+name: 'App'
+}
+</script>
