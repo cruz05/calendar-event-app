1a1b89aed56e8d1ba3a65d2a641b2e3ae0344e40
Daiana <dauipoma@gmail.com>
2023-04-27 20:57:52 +0200

    Add new components

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,25 @@
+<template>
+<div id="calendar-day">
+    <div>
+        <div>{{ day.abbvTitle }}</div>
+        <div>{{ day.id }}</div>
+        <div>
+            <CalendarEvent v-if="day.events.length > 0" v-for="(event, index) in day.events"
+            :key="index"
+            :event="event"
+            :day="day"/>
+
+            <p v-else>No events</p>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+import CalendarEvent from './CalendarEvent.vue';
+
+export default{
+    name: "CalendarDay",
+    props: ["day"],
+    components: { CalendarEvent }
+}
+</script>
